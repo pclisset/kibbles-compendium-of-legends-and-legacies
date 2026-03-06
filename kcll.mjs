9ecb60351038bdf5a2597af042e6c4b8235acffe
pclisset-admin <pclisset@gmail.com>
2026-03-05 22:56:36 -0500

    1. Added level 10 ASI bonus to warden class 2. Added mystic bulwark calculation type to armor class recipes in init hook 3. exposed equipped armor max dex property in getRollData hook for mystic bulwark calculation
    Everything needs testing

--- kcll.mjs
+++ kcll.mjs
@@ -24,4 +24,27 @@ Hooks.once("init", () => {
     _id: dnd5e.utils.staticID("dnd5edazed"),
     reference: "Compendium.kibbles-compendium-of-legends-and-legacies.kcll-journals.JournalEntry.NXibCQQFLkrAGE1i.JournalEntryPage.8t64sXFlxtCVP3b1",
   };
-})
+
+  // Register the Mystic Bulwark AC calculation mode.
+  CONFIG.DND5E.armorClasses.wardenMysticBulwark = {
+    label: "KCLL.ArmorClassMysticBulwark",
+    formula: "@attributes.ac.armor + min(@abilities.wis.mod, @attributes.ac.maxDex)"
+  };
+});
+
+/**
+ * Inject the equipped armor's maximum dexterity bonus into roll data as
+ * @attributes.ac.maxDex. This allows KCLL features to reference the armor's
+ * dex cap.
+ *
+ * When no armor is equipped, maxDex is set to 99
+ */
+Hooks.on("dnd5e.getRollData", (actor, rollData) => {
+  const equippedArmor = actor.items.find(item =>
+    item.type === "equipment" &&
+    item.system.equipped === true &&
+    ["light", "medium", "heavy"].includes(item.system.armor?.type)
+  );
+  const maxDex = equippedArmor?.system.armor.dex ?? 99;
+  foundry.utils.setProperty(rollData, "attributes.ac.maxDex", maxDex);
+});
